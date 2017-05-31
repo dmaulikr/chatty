3a3f0a92b31d6ca4ff93a5ca550cf41ab2c27c24
Simon Tucker <srtucker22@gmail.com>
2017-05-31 15:48:59 -0500

    Step 4.9: Add CREATE_MESSAGE_MUTATION to Messages

--- client/src/screens/messages.screen.js
+++ client/src/screens/messages.screen.js
@@ -13,6 +13,7 @@ import { graphql, compose } from 'react-apollo';
 import Message from '../components/message.component';
 import MessageInput from '../components/message-input.component';
 import GROUP_QUERY from '../graphql/group.query';
+import CREATE_MESSAGE_MUTATION from '../graphql/create-message.mutation';
 
 const styles = StyleSheet.create({
   container: {
@@ -62,8 +63,11 @@ class Messages extends Component {
   }
 
   send(text) {
-    // TODO: send the message
-    console.log(`sending message: ${text}`);
+    this.props.createMessage({
+      groupId: this.props.navigation.state.params.groupId,
+      userId: 1, // faking the user for now
+      text,
+    });
   }
 
   keyExtractor = item => item.id;
@@ -108,6 +112,14 @@ class Messages extends Component {
 }
 
 Messages.propTypes = {
+  createMessage: PropTypes.func,
+  navigation: PropTypes.shape({
+    state: PropTypes.shape({
+      params: PropTypes.shape({
+        groupId: PropTypes.number,
+      }),
+    }),
+  }),
   group: PropTypes.shape({
     messages: PropTypes.array,
     users: PropTypes.array,
@@ -126,6 +138,16 @@ const groupQuery = graphql(GROUP_QUERY, {
   }),
 });
 
+const createMessageMutation = graphql(CREATE_MESSAGE_MUTATION, {
+  props: ({ mutate }) => ({
+    createMessage: ({ text, userId, groupId }) =>
+      mutate({
+        variables: { text, userId, groupId },
+      }),
+  }),
+});
+
 export default compose(
   groupQuery,
+  createMessageMutation,
 )(Messages);
