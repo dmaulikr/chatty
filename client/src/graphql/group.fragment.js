1f9753ffcb3135f2fbdc081bd53ed3509dd615cd
Simon Tucker <srtucker22@gmail.com>
2017-06-10 10:30:33 +0500

    Step 8.1: Create GROUP_FRAGMENT

---
+++ client/src/graphql/group.fragment.js
@@ -0,0 +1,20 @@
+import gql from 'graphql-tag';
+
+import MESSAGE_FRAGMENT from './message.fragment';
+
+const GROUP_FRAGMENT = gql`
+  fragment GroupFragment on Group {
+    id
+    name
+    users {
+      id
+      username
+    }
+    messages(limit: $limit, offset: $offset) {
+      ... MessageFragment
+    }
+  }
+  ${MESSAGE_FRAGMENT}
+`;
+
+export default GROUP_FRAGMENT;
