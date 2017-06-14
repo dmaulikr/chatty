aa4e77becbe812c60b11faa365f601a09e4040a7
Simon Tucker <srtucker22@gmail.com>
2017-06-14 09:53:31 +0500

    Step 11.7: Add FIREBASE_SERVER_KEY to .env and config

--- server/config.js
+++ server/config.js
@@ -2,6 +2,4 @@ import dotenv from 'dotenv';
 
 dotenv.config({ silent: true });
 
-export const { JWT_SECRET } = process.env;
-
-export default JWT_SECRET;
+export const { JWT_SECRET, FIREBASE_SERVER_KEY } = process.env;
