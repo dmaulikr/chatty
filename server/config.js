82d8ec9ab6b968b73c3e9fd80d2e165267bf0e35
Simon Tucker <srtucker22@gmail.com>
2017-06-23 23:59:57 +0500

    Step 12.4: Add AWS credentials to config

--- server/config.js
+++ server/config.js
@@ -2,4 +2,11 @@ import dotenv from 'dotenv';
 
 dotenv.config({ silent: true });
 
-export const { JWT_SECRET, FIREBASE_SERVER_KEY } = process.env;
+export const {
+  JWT_SECRET,
+  FIREBASE_SERVER_KEY,
+  AWS_BUCKET,
+  AWS_ACCESS_KEY_ID,
+  AWS_SECRET_ACCESS_KEY,
+  AWS_REGION,
+} = process.env;
